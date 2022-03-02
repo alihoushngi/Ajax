5226acd7940379ef3c72377847cd7830d5c350d9
alihoushngi <62546675+alihoushngi@users.noreply.github.com>
2022-03-02 18:12:20 +0330

    end learning in chapter one

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // Ajax
+document.querySelector("#button").addEventListener("click", showContent);
+
+function showContent() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "data.txt", true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      document.querySelector("#output").innerHTML = `<h1>${this.response}</h1>`;
+    }
+  };
+
+  xhr.send();
+}
